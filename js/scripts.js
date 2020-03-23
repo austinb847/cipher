c8739bb1ab0b17f696fcf283ee8c32ced6721584
austinb847 <austinb847@icloud.com>
2020-03-23 16:18:49 -0700

    add function to reverse string

--- js/scripts.js
+++ js/scripts.js
@@ -9,8 +9,22 @@ var firstLastLetterCaps = function(userSentence) {
   return newString;
 }
 
+var reverseString = function(newString) {
+  var array, revStr;
+
+  array = newString.split("");
+  array.reverse();
+
+  revStr = array.join("");
+
+  return revStr;
+}
+
 var userSentence = prompt("Enter a sentence:");
 console.log(userSentence);
 
 var b = firstLastLetterCaps(userSentence);
 alert(b);
+
+var c = reverseString(b);
+alert(c);
